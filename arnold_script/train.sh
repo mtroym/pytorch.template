8d2c95afc92612a7e18576da594b341049b07248
MTonyM <maoymt@gmail.com>
2019-02-12 14:15:32 +0800

    update training script.

--- arnold_script/train.sh
+++ arnold_script/train.sh
@@ -14,21 +14,18 @@ cd /opt/tiger/data/
 tar -xf segTHOR.tar
 cd $THIS_DIR
 cd ..
-ls
-echo "done decompressing!
-"
 echo "arguements:
 ---------"
 echo "${@}"
 echo "----------"
 exec $@
-python3 main.py --data ../data/ --batchSize 1 --debug True --epochNum 0 --netType  deeplab --dataset segTHOR --metrics '[]'
+echo "\$GEN=${GEN}, \$MODELS=${MODELS}, \$WWW=${WWW}"
+#python3 main.py --data ../data/ --batchSize 1 --debug True --epochNum 0 --netType  deeplab --dataset segTHOR --metrics '[]'
 # Do like this.
 # python3 main.py --data ./data/ --gen /mnt/cephfs/lab/tony/gen/  \
 #                                --resume ./models/               \
 #                                --www /mnt/cephfs/lab/tony/www/  \
 #                                --epochNum 0 --batchSize 64      \
-
 # & mv ../models/* /mnt/cephfs/lab/tony/models/
 #python main.py --data ../data/ \
 #               --gen ../gen/ \
