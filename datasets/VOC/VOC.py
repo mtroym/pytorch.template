5c3eea7504c289978b888a18963b33a1da7d8e87
MTonyM <maoymt@gmail.com>
2019-03-11 05:27:41 +0800

    sync

--- datasets/VOC/VOC.py
+++ datasets/VOC/VOC.py
@@ -1,17 +1,19 @@
-import cv2
-import torch
-import numpy as np
-import datasets.transforms as t
-from torch.utils.data import Dataset
-from datasets.VOC.decodeseg import encode_segmap
 from PIL import Image
+import albumentations as albu
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+import datasets.transforms as t
+from datasets.VOC.decodeseg import encode_segmap
+# https://github.com/nyoki-mtl/pytorch-segmentation/blob/master/src/train.py
 
 def bgr2rgb(im):
     b = im[..., 0]
     g = im[..., 1]
     r = im[..., 2]
-    return np.stack([r,g,b], 2)
-
+    return np.stack([r, g, b], 2)
 
 
 class VOC(Dataset):
@@ -23,6 +25,7 @@ class VOC(Dataset):
         self.opt = opt
         self.split = split
         self.dir = imageInfo['basedir']
+        self.ignore_index = 255
         self.class_names = np.array([
             'background',
             'aeroplane',
@@ -48,18 +51,26 @@ class VOC(Dataset):
         ])
         self.mean_bgr = np.array([0.485, 0.456, 0.406])
         self.var = np.array([0.229, 0.224, 0.225])
+        self.target_size = (self.inputSize[0] + 1, self.inputSize[1] + 1)
+        if split == 'train':
+            self.resizor = albu.Compose([albu.RandomScale(scale_limit=(-0.5, 0.5), p=1.0),
+                                         t.PadIfNeededRightBottom(min_height=self.target_size[0],
+                                                                  min_width=self.target_size[1],
+                                                                  value=0, ignore_index=self.ignore_index, p=1.0),
+                                         albu.RandomCrop(height=self.target_size[0], width=self.target_size[1],
+                                                         p=1.0)])
+        else:
+            self.resizor = albu.Compose([t.PadIfNeededRightBottom(min_height=self.target_size[0],
+                                                                  min_width=self.target_size[1], value=0,
+                                                                  ignore_index=self.ignore_index, p=1.0),
+                                         albu.Crop(x_min=0, x_max=self.target_size[1], y_min=0,
+                                                   y_max=self.target_size[0])])
 
     def __getitem__(self, index):
         path, target = self.imageInfo[index]
-        image = cv2.imread(path)
-        image = np.asarray(image)
+        image = np.array(Image.open(path))
+        label = np.array(Image.open(target))
 
-        target = cv2.imread(target)
-        target = bgr2rgb(target)
-
-        # fix b ugs.
-        image = cv2.resize(image, self.inputSize, cv2.INTER_AREA)
-        target = cv2.resize(target, self.inputSize, cv2.INTER_AREA)
 
         target = encode_segmap(target)
 
@@ -69,10 +80,9 @@ class VOC(Dataset):
 
         return image, target
 
- 
     def __len__(self):
         return len(self.imageInfo)
- 
+
     def preprocess(self, im, xml):
         """
         :param im: np.array of size(h, w, c)
@@ -86,12 +96,11 @@ class VOC(Dataset):
         #
         # if self.split == 'train':
         #     im = t.addNoise(im, 0.001, 0.001)
-            # im, xml = t.randomSizeCrop(im, xml, 0.9)
+        # im, xml = t.randomSizeCrop(im, xml, 0.9)
         #  >>> how to resize....
 
         im = np.transpose(im, (2, 0, 1))
         return im, xml
-
 
     def postprocess(self, im, xml):
         """
@@ -107,7 +116,6 @@ class VOC(Dataset):
         return im, xml
 
 
-
 def getInstance(info, opt, split):
     myInstance = VOC(info, opt, split)
     opt.inputSize = myInstance.inputSize
@@ -115,15 +123,15 @@ def getInstance(info, opt, split):
     return myInstance
 
 
-
 def main():
     img_gray = np.eye(100)
-    img_gray[1:90,10:20] = 2
+    img_gray[1:90, 10:20] = 2
     print(np.unique(img_gray))
 
     im = cv2.resize(img_gray, (50, 50), cv2.INTER_AREA)
     print(np.unique(im))
     print(im.shape)
+
 
 if __name__ == '__main__':
     main()
