542d08db2d21200c64cf4dee8669bd1bb931ac11
MTonyM <maoymt@gmail.com>
2019-03-24 18:23:27 +0800

    update DAVIS.

--- datasets/DAVIS/DAVIS-gen.py
+++ datasets/DAVIS/DAVIS-gen.py
@@ -1,16 +1,41 @@
 import os
-
+import  numpy as np
 import torch
+from PIL import Image
 
 
 def exec(opt, cacheFilePath):
     assert os.path.exists(opt.data), 'Data directory not found: ' + opt.data
     info = {
         'base_dir': "",
-        'train': "",
-        'val': ""
+        'train': processing(cacheFilePath, 'train'),
+        'val': processing(cacheFilePath, 'val'),
+        # 'test': processing(cacheFilePath, 'test')
     }
     print("************* Generating list of data ....**************")
     print("^^^^^^^^^^^^ Saved the cached file in {} ^^^^^^^^^^^^^".format(cacheFilePath))
     torch.save(info, cacheFilePath)
     return info
+
+
+def processing(path, split):
+    folders_all = []
+    for yr in [2016, 2017]:
+        image_set_path = os.path.join(path, 'ImageSet', str(yr), split + '.txt')
+        with open(image_set_path, 'r') as f:
+            folders = f.read()
+            folders = folders.split()
+            folders_all += folders
+    instance_all = []
+    for video in folders_all:
+        image_path = os.path.join(path, 'JPEGImages', video)
+        gt_path = os.path.join(path, 'Annotations', video)
+        ground_truth = np.array(Image.open(gt_path))
+        instance_num = np.unique(ground_truth).__len__()
+        if np.all(ground_truth == 0):
+            continue
+        for i in range(instance_num):
+            if i == 0:
+                continue
+            instance_all.append((image_path, gt_path, i))
+    return instance_all
--- datasets/DAVIS/DAVIS.py
+++ datasets/DAVIS/DAVIS.py
@@ -1,24 +1,35 @@
+from PIL import Image
 import cv2
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-import datasets.transforms as t
-
+import albumentations as albu
 
 class DAVIS(Dataset):
     def __init__(self, info, opt, split):
-        self.input_size = (122, 122)
-        raise NotImplementedError()
+        self.input_size = (480, 256)
+        self.instance_all = info[split]
 
     def __len__(self):
-        raise NotImplementedError()
+        return len(self.instance_all)
 
     def __getitem__(self, i):
-        raise NotImplementedError()
+        image_path, gt_path, instance_num = self.instance_all[i]
+        image = Image.open(image_path)
+        ground_truth = np.array(Image.open(gt_path))
+        label = np.zeros_like(ground_truth)
+        label[ground_truth == instance_num] = 1
+
+        # TODO; label distortion.
+
+        return image, label
+
+
+
 
 def getInstance(info, opt, split):
     myInstance = DAVIS(info, opt, split)
     # print(myInstance.inputSize)
-    opt.inputSize = myInstance.inputSize
+    opt.inputSize = myInstance.input_size
     return myInstance
