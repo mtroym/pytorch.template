cb808fe03080cfab88160831fc647c1c022b2829
MTonyM <maoymt@gmail.com>
2019-04-11 15:23:02 +0800

    [segTHOR]: fix bugs

--- models/backbones/__init__.py
+++ models/backbones/__init__.py
@@ -1,5 +1,6 @@
 from models.backbones import Xception, Resnet
 
+
 def build_backbone(backbone, output_stride, BatchNorm):
     if backbone == 'Xception':
         return Xception.AlignedXception(output_stride, BatchNorm, pretrained=False)
