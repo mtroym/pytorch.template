58d12c573e96b0098cd3da13e1129744da937f72
MTonyM <maoymt@gmail.com>
2019-04-22 06:13:18 +0800

    add model infos. debug/

--- models/deeplab/deeplab.py
+++ models/deeplab/deeplab.py
@@ -268,12 +268,12 @@ def createModel(opt):
     k = 0
     for i in params:
         l = 1
-        print("该层的结构：" + str(list(i.size())))
+        print("structure:" + str(list(i.size())))
         for j in i.size():
             l *= j
-        print("该层参数和：" + str(l))
+        print("total params in this l.#:" + str(l))
         k = k + l
-    print("总参数数量和：" + str(k))
+    print("total params:" + str(k))
     if opt.GPU:
         model = model.cuda()
     return model
