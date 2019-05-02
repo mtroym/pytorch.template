0d0ce1dfca44d259e4ce8415b90d44f53290e1fc
MTonyM <maoymt@gmail.com>
2019-05-03 02:10:04 +0800

    debug...

--- datasets/segTHOR3D/segTHOR3D-gen.py
+++ datasets/segTHOR3D/segTHOR3D-gen.py
@@ -41,19 +41,13 @@ def load_paths(opt, base, pa, split):
         img_path = os.path.join(base, patients, patients + '.nii.gz')
         gt = nib.load(gt_path).get_data()
         for i in range(gt.shape[2]):
-            if np.sum(gt[:, :, i]) == 0 and split == 'train':
-                continue
             Z.append(((patients, i), gt_path, img_path))
         for i in range(gt.shape[0]):
             if i <  85 or i  > 401:
                 continue
-            if np.sum(gt[i, :, :]) == 0 and split == 'train':
-                continue
             X.append(((patients, i), gt_path, img_path))
         for i in range(gt.shape[1]):
             if i < 162 or i > 413:
-                continue
-            if np.sum(gt[:, i, :]) == 0 and split == 'train':
                 continue
             Y.append(((patients, i), gt_path, img_path))
         bar.update(idx + 1)
