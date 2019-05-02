21540b50bfb1369c0577139c2265addf2afe132d
MTonyM <maoymt@gmail.com>
2019-05-03 00:24:38 +0800

    add deeplabz3d.  test sync

--- train.sh
+++ train.sh
@@ -1,10 +1,9 @@
 python3 main.py --data /root/data \
 --batchSize 12 --epochNum 0 \
---netType deeplabz --dataset segTHOR \
+--netType deeplabz3d --dataset segTHOR \
 --metrics '[Dice]' --LR 0.0005 \
 --nThreads 5 --nEpochs 100 \
 --logDir ../log/ --debug F \
 --logNum 1 --optimizer SGD \
---suffix BS24 \
 --backbone Resnet \
 --DSmode file --epochNum -1
