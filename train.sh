48ea99a71d004edf469328a43e9c05367068b67e
MTonyM <maoymt@gmail.com>
2019-05-02 18:21:13 +0000

    sync

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python3 main.py --data /root/data \
 --batchSize 12 --epochNum 0 \
 --netType deeplabz3d --dataset segTHOR3D \
 --metrics '[Dice]' --LR 0.0005 \
---nThreads 5 --nEpochs 100 \
+--nThreads 30 --nEpochs 100 \
 --logDir ../log/ --debug F \
 --logNum 1 --optimizer SGD \
 --backbone Xception \
--- trainZR.sh
+++ trainZR.sh
@@ -2,7 +2,7 @@ python3 main.py --data /root/data \
 --batchSize 20 --epochNum -1 \
 --netType deeplabz --dataset segTHOR \
 --metrics '[Dice]' --LR 0.0005 \
---nThreads 10 --nEpochs 200 \
+--nThreads 30 --nEpochs 200 \
 --logDir ../log/ --debug F \
 --logNum 1 --optimizer SGD \
 --suffix BS24 \
