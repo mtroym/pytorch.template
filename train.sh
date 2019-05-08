ce70c297b69c7403874a90f3bde6c4245199f27b
MTonyM <maoymt@gmail.com>
2019-05-08 21:05:54 +0000

    sync

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 python3 main.py --data /root/data \
 --batchSize 6 --epochNum 0 \
 --netType deeplabz3d --dataset segTHOR3D \
---metrics '[]' --LR 0.0006 \
---nThreads 30 --nEpochs 100 \
+--metrics '[]' --LR 0.0008 \
+--nThreads 30 --nEpochs 110 \
 --logDir ../log/ --debug F \
 --logNum 1 --optimizer SGD \
 --backbone Xception \
